4730169b64daa8c255a0886c7a538542d5464313
juliarose <julialunarose@gmail.com>
2021-05-16 21:18:48 -0400

    release v1.0.0

--- package.sh
+++ package.sh
@@ -9,6 +9,7 @@ cp -a ./css ./dist/trade.links/css
 cp -a ./views ./dist/trade.links/views
 cp -a ./manifest.json ./dist/trade.links/manifest.json
 cp -a ./README.md ./dist/trade.links/README.md
+cp -a ./LICENSE ./dist/trade.links/LICENSE
 
 cd ./dist/trade.links
 
